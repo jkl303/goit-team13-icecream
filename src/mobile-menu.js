899442967d5301fbf08c39d0213c0fa51fff9b3b
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-07-23 14:21:19 +0300

    header-mobile-menu
    Сделал меню. Сейчас буду заниматься сплыванием окна справа и модальным окном.

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -10,7 +10,7 @@
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('burger-hidden');
     refs.body.classList.toggle('no-scroll');
   }
 })();
